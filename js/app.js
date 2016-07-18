333873264fe037107fbd63adecf80079835b8f28
dancastillo <dancastillo99@gmail.com>
2016-07-18 18:29:31 -0400

    toggle terminal

---
+++ js/app.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+	//alert("this works");
+	$('#toggleTerminal').click(function() {
+		//alert("this works");
+		$('#terminalDiv').toggle();
+	});
+});
--- js/script.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function() {
-	//alert("this works");
-    $(".pswSections").fadeIn(3000);
-});
