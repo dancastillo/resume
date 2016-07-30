a1427909b9d2dc1d9867fea6afe32aff50bb5fc5
Dan Castillo <dancastillo99@gmail.com>
2016-07-29 21:15:26 -0400

    browser testing

--- js/app.js
+++ js/app.js
@@ -1,19 +1,18 @@
-let currentWeather;
-$(document).ready(function() {
-	//alert("this works");
-	$('#toggleTerminal').click(function() {
-		//alert("this works");
-		$('#terminalDiv').toggle();
-	});
+var currentWeather;
+$(document).ready(function(){
+  $("#toggleTerminal").click(function(e){
+    $("#terminalDiv").slideToggle("slow");
+    e.preventDefault();
+  });
 	
 	$.ajax({
         	url: "https://api.wunderground.com/api/26ef3640837b68be/conditions/q/CT/New%20Haven.json",
         	dataType : "jsonp",
         	success : function(parsed_json) {
-            	let city = parsed_json['current_observation']['display_location']['city'];
-        	    let state_name = parsed_json['current_observation']['display_location']['state_name'];
-    	        let temperature_string = parsed_json['current_observation']['temperature_string'];
-	            let weather = parsed_json['current_observation']['weather'];
+            	var city = parsed_json['current_observation']['display_location']['city'];
+        	    var state_name = parsed_json['current_observation']['display_location']['state_name'];
+    	        var temperature_string = parsed_json['current_observation']['temperature_string'];
+	            var weather = parsed_json['current_observation']['weather'];
             	currentWeather = "The tempature is " + temperature_string + " and it is currently " + weather + " for " + city + ", " + state_name;
         	    $('#currentWeather').text(currentWeather);
     	    }
