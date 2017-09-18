f59a5a4f6df9997a88bb99045b579dcc5d6dbcfa
dancastillo <dancastillo99@gmail.com>
2017-09-18 18:27:03 -0400

    app.js: clean up app.js
    Reformatted spacing removed commented out console.log

--- js/app.js
+++ js/app.js
@@ -4,24 +4,23 @@ $(document).ready(function(){
     $("#terminalDiv").slideToggle("slow");
     e.preventDefault();
   });
-	
-	$.ajax({
-        	url: "https://api.wunderground.com/api/26ef3640837b68be/conditions/q/CT/New%20Haven.json",
-        	dataType : "jsonp",
-        	success : function(parsed_json) {
-            	var city = parsed_json['current_observation']['display_location']['city'];
-        	    var state_name = parsed_json['current_observation']['display_location']['state_name'];
-    	        var temperature_string = parsed_json['current_observation']['temperature_string'];
-	            var weather = parsed_json['current_observation']['weather'];
-            	currentWeather = "The tempature is " + temperature_string + " and it is currently " + weather + " for " + city + ", " + state_name;
-        	    $('#currentWeather').text(currentWeather);
-    	    }
-	    }); 
-	$('#getWeather').click(function() {
-		//alert("this works");
-
-	 	$('#displayWeather').show().delay(5000).fadeOut();
-		
-	});
-	
+  
+  $.ajax({
+    url: "https://api.wunderground.com/api/26ef3640837b68be/conditions/q/CT/New%20Haven.json",
+    dataType : "jsonp",
+    success : function(parsed_json) {
+      var city = parsed_json['current_observation']['display_location']['city'];
+      var state_name = parsed_json['current_observation']['display_location']['state_name'];
+      var temperature_string = parsed_json['current_observation']['temperature_string'];
+      var weather = parsed_json['current_observation']['weather'];
+      currentWeather = "The tempature is " + temperature_string + " and it is currently " + weather + " for " + city + ", " + state_name;
+      $('#currentWeather').text(currentWeather);
+    }
+  }); 
+  $('#getWeather').click(function() {
+    
+    $('#displayWeather').show().delay(5000).fadeOut();
+    
+  });
+  
 });
